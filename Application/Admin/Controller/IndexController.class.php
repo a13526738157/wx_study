21be146f3982666b72bf48a756b5cf6e539b5f21
whb <13526738157@163.com>
2016-12-27 11:49:57 +0800

    ceshi

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -6,19 +6,19 @@ class IndexController extends AdminController
 {
 	public function index(){	//name:品牌商管理
 		$weObj = $this->weObj;
-		$type = $weObj->getRev()->getRevType();
-		switch($type) {
-			case Wechat::MSGTYPE_TEXT:
-					$weObj->text("hello, I'm wechat")->reply();
-					exit;
-					break;
-			case Wechat::MSGTYPE_EVENT:
-					break;
-			case Wechat::MSGTYPE_IMAGE:
-					break;
-			default:
-			$weObj->text("help info")->reply();
+		// $type = $weObj->getRev()->getRevType();
+		// switch($type) {
+		// 	case Wechat::MSGTYPE_TEXT:
+		// 			$weObj->text("hello, I'm wechat")->reply();
+		// 			exit;
+		// 			break;
+		// 	case Wechat::MSGTYPE_EVENT:
+		// 			break;
+		// 	case Wechat::MSGTYPE_IMAGE:
+		// 			break;
+		// 	default:
+		// 	$weObj->text("help info")->reply();
 				
-			}
+		// 	}
 	}
 }
--- Application/Common/Controller/AdminController.class.php
+++ Application/Common/Controller/AdminController.class.php
@@ -11,6 +11,20 @@ class AdminController extends Controller {
 			);
 		$this->weObj = new Wechat($options);
 		$this->weObj->valid();
+		$type = $weObj->getRev()->getRevType();
+		switch($type) {
+			case Wechat::MSGTYPE_TEXT:
+					$weObj->text("hello, I'm wechat")->reply();
+					exit;
+					break;
+			case Wechat::MSGTYPE_EVENT:
+					break;
+			case Wechat::MSGTYPE_IMAGE:
+					break;
+			default:
+			$weObj->text("help info")->reply();
+				
+			}	
 	}
 
 }
