ad0c4079db5960ff33a45e0a9dc519768b8124c1
whb <13526738157@163.com>
2016-12-27 13:44:10 +0800

    ceshi

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -2,23 +2,47 @@
 namespace Admin\Controller;
 use Think\Controller;
 use Common\Controller\AdminController;
-class IndexController extends AdminController
+use Org\Wechat\wechat;
+class IndexController extends Controller
 {
-	public function index(){	//name:品牌商管理
-		$weObj = $this->weObj;
-		// $type = $weObj->getRev()->getRevType();
-		// switch($type) {
-		// 	case Wechat::MSGTYPE_TEXT:
-		// 			$weObj->text("hello, I'm wechat")->reply();
-		// 			exit;
-		// 			break;
-		// 	case Wechat::MSGTYPE_EVENT:
-		// 			break;
-		// 	case Wechat::MSGTYPE_IMAGE:
-		// 			break;
-		// 	default:
-		// 	$weObj->text("help info")->reply();
+    public function _initialize(){
+		$options = array(
+				'token'=>'weixin', //填写你设定的key
+		        'encodingaeskey'=>'dCAZhdWvBHrz2vJjBzTOgMzqeQfUdIu5exSAQdgojQa', //填写加密用的EncodingAESKey，如接口为明文模式可忽略
+		        'appid'	=>	'wx0aac34300c5a1982',
+		        'appsecret'	=>	'132d1797e84428db705eabc15b078765'
+			);
+		$this->weObj = new Wechat($options);
+		$this->weObj->valid();
+		$this->bulid_menu();
+		$type = $this->weObj->getRev()->getRevType();
+		M('test')->add(array('content'=>$type));
+		switch($type) {
+			case Wechat::MSGTYPE_TEXT:
+					$this->weObj->text("hello, I'm wechat".json_encode($this->getRevData()))->reply();
+					exit;
+					break;
+			case Wechat::MSGTYPE_EVENT:
+					break;
+			case Wechat::MSGTYPE_IMAGE:
+					break;
+			default:
+			$this->weObj->text("help info")->reply();
 				
-		// 	}
+			}	
+	}
+	private function bulid_menu(){
+		//获取菜单操作:
+	$weObj = $this->weObj;
+    $menu = $weObj->getMenu();
+    //设置菜单
+    $newmenu =  array(
+    		"button"=>
+    			array(
+    				array('type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'),
+    				array('type'=>'view','name'=>'我要搜索','url'=>'http://www.baidu.com'),
+    				)
+   		);
+   $result = $weObj->createMenu($newmenu);
 	}
 }
--- Application/Common/Controller/AdminController.class.php
+++ Application/Common/Controller/AdminController.class.php
@@ -4,46 +4,7 @@ use Think\Controller;
 use Think\Auth;
 use Org\Wechat\wechat;
 class AdminController extends Controller {
-    public function _initialize(){
-		$options = array(
-				'token'=>'weixin', //填写你设定的key
-		        'encodingaeskey'=>'dCAZhdWvBHrz2vJjBzTOgMzqeQfUdIu5exSAQdgojQa', //填写加密用的EncodingAESKey，如接口为明文模式可忽略
-		        'appid'	=>	'wx0aac34300c5a1982',
-		        'appsecret'	=>	'132d1797e84428db705eabc15b078765'
-			);
-		$this->weObj = new Wechat($options);
-		$this->weObj->valid();
-		$this->bulid_menu();
-		$type = $this->weObj->getRev()->getRevType();
-		M('test')->add(array('content'=>$type));
-		switch($type) {
-			case Wechat::MSGTYPE_TEXT:
-					$this->weObj->text("hello, I'm wechat")->reply();
-					exit;
-					break;
-			case Wechat::MSGTYPE_EVENT:
-					break;
-			case Wechat::MSGTYPE_IMAGE:
-					break;
-			default:
-			$this->weObj->text("help info")->reply();
-				
-			}	
-	}
-	private function bulid_menu(){
-		//获取菜单操作:
-	$weObj = $this->weObj;
-    $menu = $weObj->getMenu();
-    //设置菜单
-    $newmenu =  array(
-    		"button"=>
-    			array(
-    				array('type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'),
-    				array('type'=>'view','name'=>'我要搜索','url'=>'http://www.baidu.com'),
-    				)
-   		);
-   $result = $weObj->createMenu($newmenu);
-	}
+
 
 
 }
